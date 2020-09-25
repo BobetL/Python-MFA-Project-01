cb6bbb6ab9e899a2ca71deccc83c2a63d69f89a0
BobetL <49470626+BobetL@users.noreply.github.com>
2020-09-25 19:33:55 +0900

    Update LIB_MFA_EU.py

--- LIB_MFA_EU.py
+++ LIB_MFA_EU.py
@@ -68,13 +68,8 @@ Dyn_MFA_System.ProcessList.append(msc.Process(Name='Second Use', ID=8))
 Dyn_MFA_System.ProcessList.append(msc.Process(Name='Recycling', ID=9))
 
 # Define parameters.
-Values_EV_Sales = pd.read_excel(r'C:\Users\tieae\Downloads\研究相关\Matlab Research Project\MFA Model\FlowCatalog.xlsx',
+Values_EV_Sales = pd.read_excel(r'C:\Users\Users\Downloads\FlowCatalog.xlsx',
                                 sheet_name='Sheet2', usecols='B').values
-
-# r'C:\Users\tieae\Downloads\研究相关\Matlab Research Project\MFA Model\FlowCatalog.xlsx',
-#                                 sheet_name='Sheet2', usecols='B'
-# r'/Users/zhoujingwei/OneDrive/文档/Matlab Research Project/FlowCatalog.xlsx',
-#                                 sheet_name='Sheet2', usecols='B'
 
 # print(Values_EV_Sales)
 LIBs_Penetration_Rate1 = np.linspace(0.7, 0.8, 5, endpoint=False)
@@ -100,46 +95,7 @@ CarDealer2Rec_Rate = 1 - CarDealer2Rep_Rate - Loss_CarDealer_Rate
 Rep2Rec = 0.1  # 0.1 of the amount from Dismantle2Rep.
 Rep2SecondUse = 1
 SecondUse2Rec = 1  # Input value.
-# EV_LT = 10  # 10 years lifetime of an EV.
 
-# Lifetime Distribution (Uniform distribution) simulated by Queueing Problem.
-
-# InstallingTime = np.zeros(1)
-# for i in range(Model_Duration + 1):
-#     InstallingTime = np.random.uniform(Model_Time_Start, Model_Time_End)
-#     InstallingTime.sort()
-# lifetime = (6, 8, 10, 12)
-# prob = (0.1, 0.4, 0.4, 0.1)
-# WorkingTime = stats.rv_discrete(values=(lifetime, prob))
-# print(InstallingTime)
-# print(WorkingTime.rvs(size=1000))
-# StartingTime = [0 for i in range(Model_Duration + 1)]
-# FinishTime = [0 for i in range(Model_Duration + 1)]
-# print(FinishTime[0], StartingTime[0], WorkingTime.rvs(size=100)[0])
-# The first generation LIBs.
-# FinishTime[0] = StartingTime[0] + WorkingTime.rvs(size=Values_EV_Sales[0])[0]
-
-# After the first generation.
-
-# for i in range(1, len(Values_EV_Sales)):
-#     if FinishTime[i - 1] > StartingTime[i]:
-#         StartingTime[i] = FinishTime[i - 1]
-#     FinishTime[i] = StartingTime[i] + WorkingTime.rvs(size=Values_EV_Sales[i])[i]
-# print(FinishTime, StartingTime)
-
-# for i in range(Model_Duration + 1):
-#     if FinishTime[i] - StartingTime[i] == 6:
-#         LIBs2CarDealer[i] = 0.1
-#     elif FinishTime[i] - StartingTime[i] == 8:
-#         LIBs2CarDealer[i] = 0.4
-#     elif FinishTime[i] - StartingTime[i] == 10:
-#         LIBs2Dismantle[i] = 0.4
-#     elif FinishTime[i] - StartingTime[i] == 12:
-#         LIBs2CarDealer[i] = 0.1
-
-
-# print(LIBs2CarDealer, LIBs2Dismantle, LIBs_in_Market_Rate)
-# print(Values_EV_Sales, LIBs_Penetration_Rate)
 
 ParameterDict = {
     'F_In': msc.Parameter(Name='Inflow_LIBs', ID=1, P_Res=0,
@@ -233,65 +189,7 @@ Dyn_MFA_System.StockDict = {
 }
 
 Dyn_MFA_System.Initialize_FlowValues()
-# print(Dyn_MFA_System.ParameterDict['F_In'].Values)
-# print(Dyn_MFA_System.Consistency_Check())
 
-# Intermediate computations.
-# LIBs_Market = np.zeros((Model_Duration + 1, Model_Duration + 1))
-# LIBs_Market[0, 0] = Dyn_MFA_System.ParameterDict['F_In'].Values[0, 0]
-# # print(LIBs_Market)
-# for r in range(Model_Duration + 1):
-#     for c in range(r, Model_Duration + 1):
-#         LIBs_Market[r, c] = Dyn_MFA_System.ParameterDict['F_In'].Values[c - r, 0]
-# data = pd.DataFrame(LIBs_Market)
-# writer = pd.ExcelWriter('Flow_Data.xlsx')
-# data.to_excel(writer, 'Sheet_1', float_format='%.5f')
-# # writer.save()
-# Dyn_MFA_System.ParameterDict.update({
-#     'Mat_B_Market': msc.Parameter(Name='LIBs_Market_yrly', ID=16, P_Res=0,
-#                                   MetaData=None, Indices='e',
-#                                   Values=LIBs_Market, Unit='1')
-# })
-
-# LIBs2CarDealer = np.array([0.1, 0.4, 0.4, 0.1])
-# LIBs2Dismantle = np.array([0.1, 0.4, 0.4, 0.1])
-# LIBs2CarDealer = LIBs2CarDealer.reshape(4, 1)
-# LIBs2Dismantle = LIBs2Dismantle.reshape(4, 1)
-
-# for c in range(Model_Duration + 1):
-#     for r in range(0, c + 1):
-#         # while LIBs_Market[r, c] > 0:
-#         if c - r == 6:
-#             LIBs2CarDealer[r, c] = 0.1
-#         elif c - r == 8:
-#             LIBs2CarDealer[r, c] = 0.4
-#         elif c - r == 10:
-#             LIBs2Dismantle[r, c] = 0.4
-#         elif c - r == 12:
-#             LIBs2CarDealer[r, c] = 0.1
-#
-# print(LIBs2CarDealer, LIBs2Dismantle)
-# Dyn_MFA_System.ParameterDict.update({
-#     'LIBs2CarDealer': msc.Parameter(Name='LIBs2CarDealer', ID=2, P_Res=1,
-#                                     MetaData=None, Indices='e',
-#                                     Values=LIBs2CarDealer, Unit='1'),
-#     'LIBs2Dismantle': msc.Parameter(Name='LIBs2Dismantle', ID=3, P_Res=2,
-#                                     MetaData=None, Indices='e',
-#                                     Values=LIBs2Dismantle, Unit='1'),
-# })
-# print(np.dot(Dyn_MFA_System.ParameterDict['Mat_B_Market'].Values[3, :],
-#              Dyn_MFA_System.ParameterDict['LIBs2CarDealer'].Values[3, :]))
-#
-# Batts = np.zeros((Model_Duration + 1, 1))
-# print(Dyn_MFA_System.ParameterDict['Mat_B_Market'].Values.shape,
-#       Dyn_MFA_System.ParameterDict['LIBs2CarDealer'].Values.shape,
-#       Dyn_MFA_System.ParameterDict['LossRate_1'].Values.shape)
-# for r in range(Model_Duration + 1):
-#     Batts[r, :] = np.dot(Dyn_MFA_System.ParameterDict['LossRate_1'].Values[r, :],
-#                          np.dot(Dyn_MFA_System.ParameterDict['Mat_B_Market'].Values[r, :],
-#                                 Dyn_MFA_System.ParameterDict['LIBs2CarDealer'].Values[r, :]))
-#
-# print(Batts)
 
 Rate_Batts = np.zeros((Model_Duration + 1, 2))
 LIBs2Dismantle = np.zeros((Model_Duration + 1, 1))
